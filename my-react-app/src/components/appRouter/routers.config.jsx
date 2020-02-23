6025ca6436c309fd0a8860c31b3431a263c95b28
julia30oren <juliaoren30@gmail.com>
2020-02-23 07:46:28 +0200

    ...

--- my-react-app/src/components/appRouter/routers.config.jsx
+++ my-react-app/src/components/appRouter/routers.config.jsx
@@ -15,7 +15,7 @@ import exit_icon from '../../icons/320139.png'
 export const routes = [
     { key: 0, path: "/", component: Opening },
     { key: 1, isVisible: 'navBar ', for: 'user', icon: log_icon, title: ' Login ', path: "/login", component: Login },
-    { key: 2, isVisible: 'Chenge&RegLinks', title: ' Chenge Password ', path: "/passwordChenge", component: passwordChenge },
+    { key: 2, isVisible: 'Chenge&RegLinks', title: ' Change Password ', path: "/passwordChenge", component: passwordChenge },
     { key: 3, isVisible: 'navBar', for: 'user', icon: home_icon, title: ' Home ', path: "/home", component: (props) => { const HomeWithAuth = withAuth(Home); return <HomeWithAuth {...props} /> } },
     { key: 4, isVisible: 'Chenge&RegLinks', title: ' Registration ', path: "/registration", component: Register },
     { key: 5, isVisible: 'navBar', for: 'adminOnly', icon: chart_icon, title: ' Chart ', path: "/chart", component: (props) => { const ChartWithAuth = withAuth(Chart); return <ChartWithAuth {...props} /> } },
